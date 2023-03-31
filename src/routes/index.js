b93e7978501e164972b7066aad6524499c6369aa
Lucas Hideki <lucashideki87@gmail.com>
2023-03-31 13:06:53 -0300

    feat: rotas em koa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import Router from '@koa/router';
+import HomeController from '@/app/controllers/HomeController';
 
 import UserController from '../app/controllers/UserController';
 
 const router = new Router();
 
-router.post('/', UserController.store);
+router.get('/', HomeController.index);
+router.post('/users', UserController.store);
 
 export default router.routes();
