662b3aafdecd903e887944b6a6192ea79ee9effd
Lucas Hideki <lucashideki87@gmail.com>
2023-03-31 13:07:04 -0300

    feat: inicializacao do koa

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import Koa from 'koa';
-import dotenv from 'dotenv';
 import CSRF from 'koa-csrf';
+import session from 'koa-session';
+import koaBody from 'koa-body';
+import koaEjs from '@koa/ejs';
+
+import dotenv from 'dotenv';
+
+import { join } from 'node:path';
 
 import sync from './database/sync';
 import routes from './routes';
@@ -12,9 +18,27 @@ async function start() {
 
   const app = new Koa();
 
+  koaEjs(app, {
+    root: join(__dirname, 'views'),
+    layout: 'template',
+    viewExt: 'html',
+    cache: false,
+    debug: true,
+  });
+
+  app.keys = ['secret'];
+
   app
+    .use(async (ctx, next) => {
+      ctx.cookies.set('SameSite', 'Strict');
+      await next();
+    })
+    .use(koaBody())
+    .use(session({}, app))
     .use(new CSRF())
     .use(routes);
+
+  app.listen(3000);
 }
 
 start();
