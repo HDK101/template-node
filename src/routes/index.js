c75b81fe142183c3fdf22a1e797747909a377b13
Lucas Hideki <lucashideki87@gmail.com>
2023-03-31 22:48:45 -0300

    feat: uso de validacao de form

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,17 @@ import Router from '@koa/router';
 import HomeController from '@/app/controllers/HomeController';
 
 import UserController from '../app/controllers/UserController';
+import validateForm from '@/app/middlewares/validateForm';
+import {object, string} from 'yup';
 
 const router = new Router();
 
+const schema = object({
+  name: string().required(),
+  password: string().required(),
+});
+
 router.get('/', HomeController.index);
-router.post('/users', UserController.store);
+router.post('/users', validateForm(schema), UserController.store);
 
 export default router.routes();
